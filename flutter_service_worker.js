8e2f2ff622ed45258589503c829a22be5db45291
Aashay Gondalia <aashaygondalia@gmail.com>
2025-10-27 12:45:02 -0400

    Update Flutter web build artifacts and base href
    Regenerated main.dart.js, flutter_bootstrap.js, and flutter_service_worker.js with updated resource hashes and service worker version. Changed index.html base href from variable to root ('/').

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d70fa93389b304135aa3d48669c3c34c",
+const RESOURCES = {"flutter_bootstrap.js": "304be8036e6e4e500762248c5d1586e9",
 "version.json": "f9a48e8bb45d446edbf62f9105be2cd5",
-"index.html": "b97a861088f7225789b204f1e4328d8b",
-"/": "b97a861088f7225789b204f1e4328d8b",
-"main.dart.js": "b566cd4c28d19a230388adbe17df90c8",
+"index.html": "c1e13f6fbdf9b0c7b2b740d198049cae",
+"/": "c1e13f6fbdf9b0c7b2b740d198049cae",
+"main.dart.js": "38176636ae36585fd148190e2685eaca",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
